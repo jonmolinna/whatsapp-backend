bc0c95db76313890e1fbbfc660c05c777682a4fa
jonmolinna <jmolina2624@gmail.com>
2022-01-21 11:14:32 -0500

    Add Delete Message

--- src/controllers/message.controller.js
+++ src/controllers/message.controller.js
@@ -1,53 +1,143 @@
-import Message from '../models/Message.js';
-import User from '../models/User.js';
+import Message from "../models/Message.js";
+import User from "../models/User.js";
 
 // Crear un Mensaje
 export const createMessage = async (req, res) => {
-    const { to, content } = req.body;
-    const errors = {};
+  const { to, content } = req.body;
+  const errors = {};
 
+  try {
+    const recipient = await User.findOne({ username: to });
+
+    if (!recipient) throw (errors.message = "El usuario no existe");
+    if (recipient.username === req.userToken.username)
+      throw (errors.message = "No puedes enviar mensaje a ti mismo");
+    if (content.trim() === "") throw (errors.message = "Ingrese un mensaje");
+
+    await Message.create({
+      message: content,
+      from: req.userToken.username,
+      to: to,
+      status: false,
+      private: true,
+    });
+
+    return res.status(200).json({ msg: "Se creo un mensaje" });
+  } catch (err) {
+    return res.status(500).json({ error: err });
+  }
+};
+
+// Obtener Mensajes por Usuario
+export const getMessages = async (req, res) => {
+  const { to } = req.query;
+  const errors = {};
+
+  try {
+    const otherUser = await User.findOne({ username: to });
+
+    if (!otherUser) throw (errors.message = "Usuario no encontrado");
+
+    const users = [req.userToken.username, otherUser.username];
+
+    const messages = await Message.find({
+      from: { $in: users },
+      to: { $in: users },
+    }).sort({ createdAt: "desc" });
+
+    return res.status(200).json({ messages });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ error: err });
+  }
+};
+
+// Obtener usuarios - grupos y sus ultimos mensajes
+export const getUsersLastMessage = async (req, res) => {
     try {
-        const recipient = await User.findOne({ username: to });
+        let users = await User.find(
+            { _id: { $ne: req.userToken.id}},
+            { password: 0, updatedAt: 0 }
+        );
 
-        if (!recipient) throw errors.message = "El usuario no existe";
-        if (recipient.username === req.userToken.username) throw errors.message = 'No puedes enviar mensaje a ti mismo';
-        if (content.trim() === "") throw errors.message = "Ingrese un mensaje";
+        let allUserMessages = await Message.find(
+            { $or: [
+                { from: req.userToken.username },
+                { to: req.userToken.username },
+            ]}
+        ).sort({'createdAt': 'desc'});
 
-        await Message.create({
-            message: content,
-            from: req.userToken.username,
-            to: to,
-            status: false,
-            private: true,
+        let usersMessage = [];
+
+        users = users.map(otherUser => {
+            let latestMessage = allUserMessages.find(
+                m => m.from === otherUser.username || m.to === otherUser.username
+            )
+            let newUsuario = {
+                _id: otherUser._id,
+                name: otherUser.name,
+                username: otherUser.username,
+                createdAt: otherUser.createdAt,
+                latestMessage: latestMessage? latestMessage : null 
+            };
+            usersMessage.push(newUsuario);
         });
 
-        return res.status(200).json({ msg: 'Se creo un mensaje' });
+        return res.status(200).json({ users:  usersMessage });
     } catch (err) {
         return res.status(500).json({ error: err });
     }
 };
 
-// Obtener Mensajes
-export const getMessages = async (req, res) => {
-    const { to } = req.query;
-    const errors = {};
 
-    try {
-        const otherUser = await User.findOne({ username: to });
+// Obtener usuario y el ultimo fecha de conexcion
+export const getUserLastTime = async (req, res) => {
+  const { to } = req.query;
 
-        if (!otherUser) throw errors.message = "Usuario no encontrado";
+  try {
+    let message = await Message.find({ from: to }).sort({'updatedAt': 'desc'}).limit(1);
+    let user = await User.find({ username: to });
+    const time = {};
 
-        const users = [req.userToken.username, otherUser.username];
-
-        const messages = await Message.find({
-            from: { $in: users },
-            to: { $in: users },
-        })
-        .sort({ 'createdAt': 'desc'});
-
-        return res.status(200).json({ messages });
-    } catch (err) {
-        console.log(err);
-        return res.status(500).json({ error: err });
+    if (message.length > 0) {
+      time.updatedAt = message[0].updatedAt
+      time.username = message[0].from
+    } else {
+      time.updatedAt = user[0].updatedAt,
+      time.username = user[0].username
     }
+
+    return res.status(200).json({ message: time });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ error: err });
+  }
+};
+
+
+// Delete Mensaje
+export const deleteMessage = async (req, res) => {
+  const { idMessage } = req.query;
+  const username = req.userToken.username;
+  try {
+    const message = await Message.findOneAndUpdate(
+      { $and: [
+        { _id: idMessage },
+        { from: username },
+      ]},
+      {
+        status: true
+      }
+    );
+
+    if (message){
+      return res.status(200).json({ message: "Se elimino el mensaje" });
+    }
+    else {
+      return res.status(400).json({ message: 'No tienes autorizacion' });
+    }
+
+  } catch (err) {
+    return res.status(500).json({ error: err });
+  }
 };
--- src/rutas/index.js
+++ src/rutas/index.js
@@ -3,7 +3,13 @@ const router = express.Router();
 
 import { verifyToken } from '../util/token.js';
 import { createUser, login } from '../controllers/user.controller.js';
-import { createMessage, getMessages } from '../controllers/message.controller.js';
+import {
+    createMessage,
+    getMessages,
+    getUsersLastMessage,
+    getUserLastTime,
+    deleteMessage
+} from '../controllers/message.controller.js';
 
 // Users (Auth)
 router.post('/addUser', createUser);
@@ -12,6 +18,9 @@ router.post('/login', login);
 // Messages
 router.post('/sendMessage', verifyToken, createMessage);
 router.get('/getMessages', verifyToken, getMessages);
+router.get('/getUsersLastMessage', verifyToken, getUsersLastMessage);
+router.get('/getUserLastTime', verifyToken, getUserLastTime);
+router.delete('/deleteMessage', verifyToken, deleteMessage);
 
 
 export default router;
